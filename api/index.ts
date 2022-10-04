1925793c1e73c51580fc6f29d052074b4e5d41d1
Sandeep Singh Mehra <sandysmart017@gmail.com>
2022-10-04 12:53:59 +0530

    split

--- api/index.ts
+++ api/index.ts
@@ -14,6 +14,7 @@ app.get('/', async (req: Request, res: Response) => {
     console.log("new time:", new Date())
     const date = String(new Date());
     console.log('date type: ',typeof date);
+    console.log("date split", date.split('.'));
     console.log("current time", date.substring(0, date.indexOf('.')))
     return res.status(200).json({"current_time": date.substring(0, date.indexOf('.'))});
 });
