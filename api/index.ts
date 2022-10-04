73dc95b47554222632e1b2f8985e08284ff51b0c
Sandeep Singh Mehra <sandysmart017@gmail.com>
2022-10-04 13:29:36 +0530

    new chnages

--- api/index.ts
+++ api/index.ts
@@ -12,7 +12,7 @@ dotenv.config();
 
 app.get('/', async (req: Request, res: Response) => {
     console.log("new time:", new Date())
-    const date = String(new Date());
+    // const date = String(new Date());
     // const date = new Date("2015-03-25T12:00:00-06:30");
     // // "2015-03-25T12:00:00-06:30"
     // console.log("2022-10-02T16:15:47-04:00")
@@ -23,11 +23,17 @@ app.get('/', async (req: Request, res: Response) => {
     // console.log("date split", date.split('.'));
     // console.log("current time", date.substring(0, date.indexOf('.')))
     const currentDate = new Date();
+    const year = currentDate.getUTCFullYear();
+    const month = currentDate.getUTCMonth();
+    const day = currentDate.getUTCDay();
+    const hour = currentDate.getUTCHours();
+    const minute = currentDate.getUTCMinutes();
+    const seconds = currentDate.getUTCSeconds();
+    console.log("2015-03-25T12:00:00-06:30", `${year}-${month}-${day}T${hour}:${minute}:${seconds}+00:00`)
+    // const options:any = {year: 'numeric', month: 'numeric', day: 'numeric' };
 
-    const options:any = {year: 'numeric', month: 'numeric', day: 'numeric' };
-
-    console.log(currentDate.toLocaleDateString('en-us', options));
-    return res.status(200).json({"current_time": currentDate.toLocaleDateString('en-us', options)});
+    // console.log(currentDate.toLocaleDateString('en-us', options));
+    return res.status(200).json({"current_time": `${year}-${month}-${day}T${hour}:${minute}:${seconds}+00:00`});
 });
 
 module.exports = app;
