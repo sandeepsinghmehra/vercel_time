c71f0632eed60a2c4c679c4ff07c68a5ba7afc77
Sandeep Singh Mehra <sandysmart017@gmail.com>
2022-10-04 13:01:33 +0530

    askfjs

--- api/index.ts
+++ api/index.ts
@@ -12,11 +12,17 @@ dotenv.config();
 
 app.get('/', async (req: Request, res: Response) => {
     console.log("new time:", new Date())
-    const date = String(new Date());
-    console.log('date type: ',typeof date);
-    console.log("date split", date.split('.'));
-    console.log("current time", date.substring(0, date.indexOf('.')))
-    return res.status(200).json({"current_time": date.substring(0, date.indexOf('.'))});
+    // const date = String(new Date());
+    const date = new Date(new Date);
+
+    date.setSeconds(0, 0);
+
+    console.log(date); // Thu Jul 21 2022 12:35:00 GMT+0300
+    console.log(date.toISOString()); // Seconds and milliseconds removed from date
+    // console.log('date type: ',typeof date);
+    // console.log("date split", date.split('.'));
+    // console.log("current time", date.substring(0, date.indexOf('.')))
+    return res.status(200).json({"current_time": date.toISOString()});
 });
 
 module.exports = app;
