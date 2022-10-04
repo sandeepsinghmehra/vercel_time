41534e05a89c3e4d51e03cb4791311854130b59e
Sandeep Singh Mehra <sandysmart017@gmail.com>
2022-10-04 13:08:53 +0530

    fsafsd

--- api/index.ts
+++ api/index.ts
@@ -13,10 +13,10 @@ dotenv.config();
 app.get('/', async (req: Request, res: Response) => {
     console.log("new time:", new Date())
     // const date = String(new Date());
-    const date = new Date(new Date);
-
+    const date = new Date("2015-03-25T12:00:00-06:30");
+    // "2015-03-25T12:00:00-06:30"
+    console.log("2022-10-02T16:15:47-04:00")
     date.setSeconds(0, 0);
-
     console.log(date); // Thu Jul 21 2022 12:35:00 GMT+0300
     console.log(date.toISOString()); // Seconds and milliseconds removed from date
     // console.log('date type: ',typeof date);
