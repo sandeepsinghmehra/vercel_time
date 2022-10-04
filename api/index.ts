7ba911cc0107e12682b00450914950dbdef3bfc3
Sandeep Singh Mehra <sandysmart017@gmail.com>
2022-10-04 12:39:15 +0530

    faskfjsa

--- api/index.ts
+++ api/index.ts
@@ -14,7 +14,7 @@ app.get('/time-format', async (req: Request, res: Response) => {
     console.log("new time:", new Date())
     const date = new Date();
 
-    return res.status(200).json({"current_time": date.toLocaleString().substring(0, date.toLocaleString().indexOf('.'))});
+    return res.status(200).json({"current_time": date.toString().substring(0, date.toString().indexOf('.'))});
 });
 
 module.exports = app;
