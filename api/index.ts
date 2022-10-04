2a9ab817b29fd5b87ebb46a20d56a54e1df61029
Sandeep Singh Mehra <sandysmart017@gmail.com>
2022-10-04 13:21:41 +0530

    options

--- api/index.ts
+++ api/index.ts
@@ -12,17 +12,22 @@ dotenv.config();
 
 app.get('/', async (req: Request, res: Response) => {
     console.log("new time:", new Date())
-    // const date = String(new Date());
-    const date = new Date("2015-03-25T12:00:00-06:30");
-    // "2015-03-25T12:00:00-06:30"
-    console.log("2022-10-02T16:15:47-04:00")
-    date.setSeconds(0, 0);
-    console.log(date); // Thu Jul 21 2022 12:35:00 GMT+0300
-    console.log(date.toISOString()); // Seconds and milliseconds removed from date
+    const date = String(new Date());
+    // const date = new Date("2015-03-25T12:00:00-06:30");
+    // // "2015-03-25T12:00:00-06:30"
+    // console.log("2022-10-02T16:15:47-04:00")
+    // date.setSeconds(0, 0);
+    // console.log(date); // Thu Jul 21 2022 12:35:00 GMT+0300
+    // console.log(date.toISOString()); // Seconds and milliseconds removed from date
     // console.log('date type: ',typeof date);
     // console.log("date split", date.split('.'));
     // console.log("current time", date.substring(0, date.indexOf('.')))
-    return res.status(200).json({"current_time": date.toISOString()});
+    const currentDate = new Date();
+
+    const options:any = {year: 'numeric', month: 'numeric', day: 'numeric' };
+
+    console.log(currentDate.toLocaleDateString('en-us', options));
+    return res.status(200).json({"current_time": currentDate.toLocaleDateString('en-us', options)});
 });
 
 module.exports = app;
